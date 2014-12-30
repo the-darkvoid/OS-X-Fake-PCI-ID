52f10872eaa45d2609c89582326b685d19110c83
the-darkvoid <the-darkvoid@users.noreply.github.com>
2014-12-30 21:42:30 +0400

    Initial check-in

--- IntelHDMobileGraphics/IntelHDMobileGraphics.h
+++ IntelHDMobileGraphics/IntelHDMobileGraphics.h
@@ -1,1 +1,41 @@
-/* add your code here */
+/*
+ *  Released under "The GNU General Public License (GPL-2.0)"
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *  Based on iSightDefender concept by Stephen Checkoway (https://github.com/stevecheckoway/iSightDefender)
+ */
+
+#ifndef IntelHDMobileGraphics_h
+#define IntelHDMobileGraphics_h
+
+#include <IOKit/IOService.h>
+#include <IOKit/pci/IOPCIDevice.h>
+
+class IntelHDMobileGraphics: public IOService
+{
+        OSDeclareDefaultStructors(IntelHDMobileGraphics);
+    
+        const void *mDeviceVtable;
+        const void *mStubVtable;
+    
+    public:
+        virtual bool init(OSDictionary *propTable);
+        virtual void free();
+        virtual bool start(IOService *provider);
+        virtual void stop(IOService *provider);
+};
+
+#endif
